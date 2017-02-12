615c2a0ea9158d128cf46d1dd7897ba9d35c2ad0
tbritoamorim <tbritoamorim@gmail.com>
2017-02-11 22:41:51 -0200

    Add files via upload

--- jssheet.js
+++ jssheet.js
@@ -8,16 +8,48 @@ function playSound(event) {
   const AUDIO = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   const KEY = document.querySelector(`div[data-key="${event.keyCode}"]`);
   const WORD = document.querySelector(`kbd[data-key="${event.keyCode}"]`);
-  console.log(WORD);
-  question = WORD.innerText;
-  console.log(question);
-  whatWasAsked.value += (question + " ");
   if (!AUDIO) return;
+  question = WORD.innerText.toUpperCase();
+  if (whatWasAsked.value.split(" ").length < 4) {
+    whatWasAsked.value += (question + " ");
+    checkSentence(whatWasAsked.value);
+  }
   KEY.classList.add("playing");
   AUDIO.currentTime = 0;
   AUDIO.play();
 }
+function checkSentence(sentence) {
+  switch (sentence) {
+    case "DO SHE ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DO HE ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DO IT ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DOES I ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DOES YOU ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DOES WE ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+    case "DOES THEY ":
+      alert("wrong");
+      whatWasAsked.value = "";
+      break;
+  }
+}
 const KEYS = Array.from(document.getElementsByClassName('key'));
-console.log(KEYS);
 KEYS.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
